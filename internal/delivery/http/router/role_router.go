6636a7cba9efeb2399f8e58ac46e6eba928c9863
Lata <amelatbay@gmail.com>
2025-06-01 09:57:23 +0300

    router setup added

---
+++ internal/delivery/http/router/role_router.go
@@ -0,0 +1,20 @@
+package router
+
+import (
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/latiiLA/coop-forex-server/internal/delivery/http/controller"
+	"github.com/latiiLA/coop-forex-server/internal/repository"
+	"github.com/latiiLA/coop-forex-server/internal/usecase"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+func NewRoleRouter(timeout time.Duration, db *mongo.Database, group *gin.RouterGroup) {
+	roleRepo := repository.NewRoleRepository(db, timeout)
+	roleUsecase := usecase.NewRoleUsecase(roleRepo, timeout)
+	roleController := controller.NewRoleController(roleUsecase)
+
+	group.POST("/role", roleController.AddRole)
+	group.GET("/role", roleController.GetAllRoles)
+}
---
+++ internal/delivery/http/router/router.go
@@ -0,0 +1,41 @@
+package router
+
+import (
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/latiiLA/coop-forex-server/configs"
+	"github.com/latiiLA/coop-forex-server/internal/infrastructure/middleware"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+func RouterSetup(timeout time.Duration, db *mongo.Database, router *gin.Engine){
+	publicRouter := router.Group("")
+	// All public APIS
+	NewUserRouter(timeout, db, publicRouter)
+
+	// All authenticated user routes (any logged-in user)
+	authRouter := router.Group("")
+	authRouter.Use(middleware.JwtAuthMiddleware(configs.JwtSecret))
+
+	// Admin-only routes
+	adminRouter := router.Group("")
+	adminRouter.Use(middleware.JwtAuthMiddleware(configs.JwtSecret), middleware.AuthorizeRoles("admin"))
+
+	// Branch-only routes
+	branchRouter := router.Group("")
+	branchRouter.Use(middleware.JwtAuthMiddleware(configs.JwtSecret), middleware.AuthorizeRoles("branch"))
+	
+	// Forex-user only routes
+	forexUserRouter := router.Group("")
+	forexUserRouter.Use(middleware.JwtAuthMiddleware(configs.JwtSecret), middleware.AuthorizeRoles("forexuser"))
+
+	// Manager-only routes
+	managerRouter := router.Group("")
+	managerRouter.Use(middleware.JwtAuthMiddleware(configs.JwtSecret), middleware.AuthorizeRoles("manager"))
+
+	SuperAdminRouter := router.Group("")
+	SuperAdminRouter.Use(middleware.JwtAuthMiddleware(configs.JwtSecret), middleware.AuthorizeRoles("admin"))
+	NewRoleRouter(timeout, db, SuperAdminRouter)
+}
+
