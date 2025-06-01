83db05b41c0fb5bcb3d12d5ec4c0f3b90d018279
Lata <amelatbay@gmail.com>
2025-06-01 09:56:27 +0300

    error and success response struct added

---
+++ internal/delivery/http/response/response.go
@@ -0,0 +1,10 @@
+package response
+
+type ErrorResponse struct {
+	Message string `json:"message"`
+}
+
+type SuccessResponse struct {
+	Message string      `json:"message"`
+	Data    interface{} `json:"data,omitempty"`
+}
