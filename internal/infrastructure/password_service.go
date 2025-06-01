d54bcfa6967add68f969a79c91c0175f8d849c57
Lata <amelatbay@gmail.com>
2025-06-01 09:53:03 +0300

    password hash and check added

---
+++ internal/infrastructure/password_service.go
@@ -0,0 +1,15 @@
+package infrastructure
+
+import (
+	"golang.org/x/crypto/bcrypt"
+)
+
+func HashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	return string(bytes), err
+}
+
+func CheckPasswordHash(hash, plainPassword string) error{
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(plainPassword))
+	return err
+}
