301bb8830312a232b63507b05393649d8515270f
Lata <amelatbay@gmail.com>
2025-06-01 09:58:47 +0300

    context util added to get and validate user Id from token

---
+++ internal/infrastructure/utils/contextutils.go
@@ -0,0 +1,27 @@
+package utils
+
+import (
+	"errors"
+
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+func GetUserID(c *gin.Context) (primitive.ObjectID, error) {
+    val, exists := c.Get("userID")
+    if !exists {
+        return primitive.NilObjectID, errors.New("user ID not found in context")
+    }
+
+    userIDStr, ok := val.(string)
+    if !ok {
+        return primitive.NilObjectID, errors.New("user ID is not a valid string")
+    }
+
+    userID, err := primitive.ObjectIDFromHex(userIDStr)
+    if err != nil {
+        return primitive.NilObjectID, errors.New("invalid ObjectID format")
+    }
+
+    return userID, nil
+}
