ae7aa120f7361382deeb0d711d1f3af6049c1077
Lata <amelatbay@gmail.com>
2025-07-13 17:47:46 +0300

    migration-upanddown

---
+++ internal/migration/down.go
@@ -0,0 +1,74 @@
+package migration
+
+import (
+	"context"
+	"log"
+
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+func Down(ctx context.Context, db *mongo.Database) error {
+
+	log.Println("🌱 Unseeding roles...")
+	if err := unseedRoles(ctx, db); err != nil {
+		return err
+	}
+
+	log.Println("🌱 Unseeding profiles...")
+	if err := unseedProfiles(ctx, db); err != nil {
+		return err
+	}
+
+	log.Println("🌱 Unseeding users...")
+	if err := unseedUsers(ctx, db); err != nil {
+		return err
+	}
+
+	log.Println("🌱 Unseeding countries...")
+	if err := unseedCountries(ctx, db); err != nil {
+		return err
+	}
+
+	log.Println("🌱 Unseeding customer types...")
+	if err := unseedCustomerTypes(ctx, db); err != nil {
+		return err
+	}
+
+	log.Println("🌱 Unseeding travel purposes...")
+	if err := unseedTravelPurpose(ctx, db); err != nil {
+		return err
+	}
+
+	log.Println("🌱 Unseeding currencies...")
+	if err := unseedCurrencies(ctx, db); err != nil {
+		return err
+	}
+
+	log.Println("🌱 Unseeding districts...")
+	if err := unseedDistricts(ctx, db); err != nil {
+		return err
+	}
+
+	log.Println("🌱 Unseeding processes...")
+	if err := unseedProcesses(ctx, db); err != nil {
+		return err
+	}
+
+	log.Println("🌱 Unseeding subprocesses...")
+	if err := unseedSubprocesses(ctx, db); err != nil {
+		return err
+	}
+
+	log.Println("🌱 Unseeding deparments...")
+	if err := unseedDepartments(ctx, db); err != nil {
+		return err
+	}
+
+	log.Println("📁 Running InitDown...")
+	if err := InitUp(ctx, db); err != nil {
+		return err
+	}
+
+	log.Println("✅ All migrations applied successfully.")
+	return nil
+}
---
+++ internal/migration/up.go
@@ -0,0 +1,73 @@
+package migration
+
+import (
+	"context"
+	"log"
+
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+func Up(ctx context.Context, db *mongo.Database) error {
+	log.Println("📁 Running InitUp...")
+	if err := InitUp(ctx, db); err != nil {
+		return err
+	}
+
+	log.Println("🌱 Seeding roles...")
+	if err := seedRoles(ctx, db); err != nil {
+		return err
+	}
+
+	log.Println("🌱 Seeding profiles...")
+	if err := seedProfiles(ctx, db); err != nil {
+		return err
+	}
+
+	log.Println("🌱 Seeding users...")
+	if err := seedUsers(ctx, db); err != nil {
+		return err
+	}
+
+	log.Println("🌱 Seeding countries...")
+	if err := seedCountries(ctx, db); err != nil {
+		return err
+	}
+
+	log.Println("🌱 Seeding customer types...")
+	if err := seedCustomerTypes(ctx, db); err != nil {
+		return err
+	}
+
+	log.Println("🌱 Seeding travel purpose...")
+	if err := seedTravelPurpose(ctx, db); err != nil {
+		return err
+	}
+
+	log.Println("🌱 Seeding currencies...")
+	if err := seedCurrencies(ctx, db); err != nil {
+		return err
+	}
+
+	log.Println("🌱 Seeding districts...")
+	if err := seedDistricts(ctx, db); err != nil {
+		return err
+	}
+
+	log.Println("🌱 Seeding processes...")
+	if err := seedProcesses(ctx, db); err != nil {
+		return err
+	}
+
+	log.Println("🌱 Seeding subprocesses...")
+	if err := seedSubprocesses(ctx, db); err != nil {
+		return err
+	}
+
+	log.Println("🌱 Seeding departments...")
+	if err := seedDepartments(ctx, db); err != nil {
+		return err
+	}
+
+	log.Println("✅ All migrations applied successfully.")
+	return nil
+}
