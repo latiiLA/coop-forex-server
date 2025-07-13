4e81fa9fd3995f21ea0c6c4a2e055df00e7242cd
Lata <amelatbay@gmail.com>
2025-07-13 17:49:21 +0300

    .env global imports

--- configs/config.go
+++ configs/config.go
@@ -1,6 +1,7 @@
 package configs
 
 import (
+	"fmt"
 	"log"
 	"os"
 	"time"
@@ -9,9 +10,18 @@ import (
 )
 
 var (
-	JwtSecret string
-	MongoURL  string
-	Timeout   time.Duration
+	JwtSecret        string
+	MongoURL         string
+	Timeout          time.Duration
+	DisableMigration string
+	FileUploadPath   string
+
+	// Ldap configs
+	LDAPHost         string
+	LDAPPort         string
+	LDAPBaseDN       string
+	LDAPBindUser     string
+	LDAPBindPassword string
 )
 
 func LoadConfig() {
@@ -23,6 +33,11 @@ func LoadConfig() {
 	MongoURL = os.Getenv("MONGO_URL")
 	if MongoURL == "" {
 		log.Fatal("Mongo url is required but not set")
+	}
+
+	DisableMigration = os.Getenv("DISABLE_MIGRATION")
+	if DisableMigration == "" {
+		log.Fatal("DisableMigration status is required but not set")
 	}
 
 	JwtSecret = os.Getenv("JWT_SECRET")
@@ -39,4 +54,36 @@ func LoadConfig() {
 	if err != nil {
 		log.Fatalf("Invalid APP_TIMEOUT format: %v", err)
 	}
+
+	FileUploadPath = os.Getenv("FILE_UPLOAD_PATH")
+	if FileUploadPath == "" {
+		log.Fatal("FILE_UPLOAD_PATH is required but not set")
+	}
+
+	LDAPHost = os.Getenv("LDAP_HOST")
+	if LDAPHost == "" {
+		log.Fatalf("LDAP Host is required but not set")
+	}
+
+	LDAPPort = os.Getenv("LDAP_PORT")
+	if LDAPPort == "" {
+		log.Fatalf("LDAP Port is required but not set")
+	}
+
+	LDAPBaseDN = os.Getenv("LDAP_BASE_DN")
+	if LDAPBaseDN == "" {
+		log.Fatalf("LDAP base DN is required but not set")
+	}
+
+	LDAPBindUser = os.Getenv("LDAP_BIND_USER")
+	if LDAPBindUser == "" {
+		log.Fatalf("LDAP bind user is required but not set")
+	}
+
+	LDAPBindPassword = os.Getenv("LDAP_BIND_PASSWORD")
+	if LDAPBindPassword == "" {
+		log.Fatalf("LDAP bind password is required but not set")
+	}
+
+	fmt.Println(LDAPHost, LDAPPort, LDAPBaseDN, LDAPBindUser, LDAPBindPassword)
 }
