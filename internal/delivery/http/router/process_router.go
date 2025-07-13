1d22aff2d3bba63c2cdccfa35f68cbeed989200d
Lata <amelatbay@gmail.com>
2025-07-13 18:10:09 +0300

    process router added

---
+++ internal/delivery/http/router/process_router.go
@@ -0,0 +1,20 @@
+package router
+
+import (
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/latiiLA/coop-forex-server/internal/delivery/http/controller"
+	"github.com/latiiLA/coop-forex-server/internal/repository"
+	"github.com/latiiLA/coop-forex-server/internal/usecase"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+func NewProcessRouter(db *mongo.Database, timeout time.Duration, group *gin.RouterGroup) {
+	processRepo := repository.NewProcessRepository(db)
+	processUsecase := usecase.NewProcessUsecase(processRepo, timeout)
+	processController := controller.NewProcessController(processUsecase)
+
+	// group.POST("/process", processController.AddProcess)
+	group.GET("/processes", processController.GetAllProcesses)
+}
