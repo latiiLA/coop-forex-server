1c3077921a2b42c5c8e9beb72ddb3f2d79500d83
Lata <amelatbay@gmail.com>
2025-07-13 18:19:27 +0300

    file model and repo interface added

--- internal/domain/model/file_model.go
+++ internal/domain/model/file_model.go
@@ -1,6 +1,7 @@
 package model
 
 import (
+	"context"
 	"time"
 
 	"go.mongodb.org/mongo-driver/bson/primitive"
@@ -11,6 +12,13 @@ type File struct {
 	URL       string             `json:"url" bson:"url"`
 	Name      string             `json:"name" bson:"name"`
 	Fid       string             `json:"fid" bson:"fid"`
+	Size      int64              `json:"size" bson:"size"`
+	MimeType  string             `json:"mime_type" bson:"mime_type"`
 	CreatedAt time.Time          `json:"created_at" bson:"created_at"`
-	UpdatedAt time.Time          `json:"updated_at" bson:"update_at"`
+	UpdatedAt time.Time          `json:"updated_at" bson:"updated_at"`
+}
+
+type FileRepository interface {
+	Create(ctx context.Context, file *File) (*primitive.ObjectID, error)
+	FindByID(ctx context.Context, file_id primitive.ObjectID) (*File, error)
 }
