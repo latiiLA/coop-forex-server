ae7fb14d8733278e2510f2dccc77e6bc5ef328b9
Lata <amelatbay@gmail.com>
2025-07-13 18:01:36 +0300

    department model updated and department repository added

--- internal/domain/model/department_model.go
+++ internal/domain/model/department_model.go
@@ -1,6 +1,7 @@
 package model
 
 import (
+	"context"
 	"time"
 
 	"go.mongodb.org/mongo-driver/bson/primitive"
@@ -8,7 +9,22 @@ import (
 
 type Department struct {
 	ID           primitive.ObjectID  `json:"_id" bson:"_id,omitempty"`
-	SubProcessID primitive.ObjectID  `json:"sub_process_id" bson:"sub_process_id"`
+	SubProcessID primitive.ObjectID  `json:"subprocess_id" bson:"subprocess_id"`
+	SubProcess   *Subprocess         `json:"subprocess,omitempty" bson:"subprocess,omitempty"`
+	Name         string              `json:"name" bson:"name"`
+	CreatedAt    time.Time           `json:"created_at" bson:"created_at"`
+	UpdatedAt    time.Time           `json:"updated_at" bson:"updated_at"`
+	CreatedBy    primitive.ObjectID  `json:"created_by" bson:"created_by"`
+	UpdatedBy    *primitive.ObjectID `json:"updated_by,omitempty" bson:"updated_by,omitempty"`
+	DeletedBy    *primitive.ObjectID `json:"deleted_by,omitempty" bson:"deleted_by,omitempty"`
+	DeletedAt    *time.Time          `json:"deleted_at,omitempty" bson:"deleted_at,omitempty"`
+	IsDeleted    bool                `json:"is_deleted" bson:"is_deleted"`
+}
+
+type DepartmentResponseDTO struct {
+	ID           primitive.ObjectID  `json:"_id" bson:"_id,omitempty"`
+	SubProcessID primitive.ObjectID  `json:"subprocess_id" bson:"subprocess_id"`
+	SubProcess   Subprocess          `json:"subprocess" bson:"subprocess"`
 	Name         string              `json:"name" bson:"name"`
 	CreatedAt    time.Time           `json:"created_at" bson:"created_at"`
 	UpdatedAt    time.Time           `json:"updated_at" bson:"update_at"`
@@ -16,5 +32,14 @@ type Department struct {
 	UpdatedBy    *primitive.ObjectID `json:"updated_by,omitempty" bson:"updated_by,omitempty"`
 	DeletedBy    *primitive.ObjectID `json:"deleted_by,omitempty" bson:"deleted_by,omitempty"`
 	DeletedAt    *time.Time          `json:"deleted_at,omitempty" bson:"deleted_at,omitempty"`
-	IsDeleted    bool                `json:"is_deleted,omitempty" bson:"is_deleted"`
+	IsDeleted    bool                `json:"is_deleted" bson:"is_deleted"`
+}
+
+type DepartmentRepository interface {
+	Create(c context.Context, department *Department) error
+	FindAll(c context.Context) ([]Department, error)
+	FindByID(c context.Context, department_id primitive.ObjectID) (*Department, error)
+	FindBySubprocessID(ctx context.Context, subprocess_id primitive.ObjectID) (*[]Department, error)
+	Update(c context.Context, department_id primitive.ObjectID, department *Department) (*Department, error)
+	Delete(c context.Context, department_id primitive.ObjectID) error
 }
