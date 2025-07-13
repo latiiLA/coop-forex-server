422ca21cf11fdfbb7516c9c17cc0067ede8cb5f9
Lata <amelatbay@gmail.com>
2025-07-13 17:57:06 +0300

    department router added

---
+++ internal/delivery/http/router/department_router.go
@@ -0,0 +1,21 @@
+package router
+
+import (
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/latiiLA/coop-forex-server/internal/delivery/http/controller"
+	"github.com/latiiLA/coop-forex-server/internal/repository"
+	"github.com/latiiLA/coop-forex-server/internal/usecase"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+func NewDepartmentRouter(db *mongo.Database, timeout time.Duration, group *gin.RouterGroup) {
+	departmentRepo := repository.NewDepartmentRepository(db)
+	departmentUsecase := usecase.NewDepartmentUsecase(departmentRepo, timeout)
+	departmentController := controller.NewDepartmentController(departmentUsecase)
+
+	// group.POST("/department", departmentController.AddDepartment)
+	group.GET("/departments/:id", departmentController.GetDepartmentsByProcessID)
+	group.GET("/departments", departmentController.GetAllDepartments)
+}
