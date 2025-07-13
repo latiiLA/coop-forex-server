d18b31580adfe4b7febe0daa7ba8217d3efbe7cc
Lata <amelatbay@gmail.com>
2025-07-13 18:03:12 +0300

    role model updated

--- internal/domain/model/role_model.go
+++ internal/domain/model/role_model.go
@@ -11,17 +11,17 @@ type Role struct {
 	ID        primitive.ObjectID  `json:"_id" bson:"_id,omitempty"`
 	Name      string              `json:"name" bson:"name"`
 	CreatedAt time.Time           `json:"created_at" bson:"created_at"`
-	UpdatedAt time.Time           `json:"updated_at" bson:"update_at"`
+	UpdatedAt time.Time           `json:"updated_at" bson:"updated_at"`
 	CreatedBy primitive.ObjectID  `json:"created_by" bson:"created_by"`
 	UpdatedBy *primitive.ObjectID `json:"updated_by,omitempty" bson:"updated_by,omitempty"`
 	DeletedBy *primitive.ObjectID `json:"deleted_by,omitempty" bson:"deleted_by,omitempty"`
 	DeletedAt *time.Time          `json:"deleted_at,omitempty" bson:"deleted_at,omitempty"`
-	IsDeleted bool                `json:"is_deleted,omitempty" bson:"is_deleted"`
+	IsDeleted bool                `json:"is_deleted" bson:"is_deleted"`
 }
 
 type RoleRepository interface {
 	Create(ctx context.Context, role *Role) error
-	FindByID(ctx context.Context, role_id primitive.ObjectID) (Role, error)
+	FindByID(ctx context.Context, role_id primitive.ObjectID) (*Role, error)
 	FindAll(ctx context.Context) ([]Role, error)
 	ExistsRoleByName(ctx context.Context, name string) (bool, error)
 }
