246946e750325970043aadeb5c81f7be610b5542
Lata <amelatbay@gmail.com>
2025-07-13 18:16:59 +0300

    currency router added

---
+++ internal/delivery/http/router/currency_router.go
@@ -0,0 +1,19 @@
+package router
+
+import (
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/latiiLA/coop-forex-server/internal/delivery/http/controller"
+	"github.com/latiiLA/coop-forex-server/internal/repository"
+	"github.com/latiiLA/coop-forex-server/internal/usecase"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+func NewCurrencyRouter(db *mongo.Database, timeout time.Duration, group *gin.RouterGroup) {
+	currencyRepository := repository.NewCurrencyRepository(db)
+	currencyUsecase := usecase.NewCurrencyUsecase(currencyRepository, timeout)
+	currencyController := controller.NewCurrencyController(currencyUsecase)
+
+	group.GET("/currencies", currencyController.GetAllCurrency)
+}
