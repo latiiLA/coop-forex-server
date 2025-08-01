726e8a4fa9dbe8e71fcb6283b7855430c06d2d51
Lata <amelatbay@gmail.com>
2025-08-01 11:19:10 +0300

    update role, delete role, get deleted roles added

--- internal/delivery/http/controller/role_controller.go
+++ internal/delivery/http/controller/role_controller.go
@@ -8,12 +8,16 @@ import (
 	"github.com/latiiLA/coop-forex-server/internal/domain/model"
 	"github.com/latiiLA/coop-forex-server/internal/infrastructure/utils"
 	"github.com/latiiLA/coop-forex-server/internal/usecase"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 type RoleController interface {
 	AddRole(c *gin.Context)
 	// GetRoleByID(c *gin.Context)
+	GetDeletedRoles(c *gin.Context)
 	GetAllRoles(c *gin.Context)
+	UpdateRole(c *gin.Context)
+	DeleteRole(c *gin.Context)
 }
 
 type roleController struct {
@@ -57,6 +61,86 @@ func (rc *roleController) AddRole(c *gin.Context) {
 
 func (rc *roleController) GetAllRoles(c *gin.Context) {
 	roles, err := rc.roleUsecase.GetAllRoles(c)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, response.ErrorResponse{Message: err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, roles)
+}
+
+func (rc *roleController) UpdateRole(c *gin.Context) {
+	authUserID, err := utils.GetUserID(c)
+	if err != nil {
+		c.JSON(http.StatusUnauthorized, response.ErrorResponse{Message: err.Error()})
+		return
+	}
+
+	// get role id from param
+	roleIDStr := c.Param("id")
+	if roleIDStr == "" {
+		c.JSON(http.StatusBadRequest, response.ErrorResponse{Message: "user ID is required"})
+		return
+	}
+
+	roleID, err := primitive.ObjectIDFromHex(roleIDStr)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, response.ErrorResponse{Message: err.Error()})
+		return
+	}
+
+	var roleUpdate model.UpdateRoleDTO
+
+	err = c.ShouldBindJSON(&roleUpdate)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, response.ErrorResponse{Message: err.Error()})
+		return
+	}
+
+	role := &model.Role{
+		Name:        roleUpdate.Name,
+		Permissions: roleUpdate.Permissions,
+	}
+
+	err = rc.roleUsecase.UpdateRole(c, authUserID, roleID, role)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, response.ErrorResponse{Message: err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, response.SuccessResponse{Message: "role updated successfully"})
+}
+
+func (rc *roleController) DeleteRole(c *gin.Context) {
+	authUserID, err := utils.GetUserID(c)
+	if err != nil {
+		c.JSON(http.StatusUnauthorized, response.ErrorResponse{Message: err.Error()})
+		return
+	}
+
+	// get role id from param
+	roleIDStr := c.Param("id")
+	if roleIDStr == "" {
+		c.JSON(http.StatusBadRequest, response.ErrorResponse{Message: "user ID is required"})
+		return
+	}
+
+	roleID, err := primitive.ObjectIDFromHex(roleIDStr)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, response.ErrorResponse{Message: err.Error()})
+		return
+	}
+
+	err = rc.roleUsecase.DeleteRole(c, authUserID, roleID)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, response.ErrorResponse{Message: err.Error()})
+	}
+
+	c.JSON(http.StatusOK, response.SuccessResponse{Message: "role deleted successfully"})
+}
+
+func (rc *roleController) GetDeletedRoles(c *gin.Context) {
+	roles, err := rc.roleUsecase.GetDeletedRoles(c)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, response.ErrorResponse{Message: err.Error()})
 		return
