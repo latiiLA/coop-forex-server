01543525a1083f73ce403288e66faf8afbbc17fc
Lata <amelatbay@gmail.com>
2025-08-01 11:29:58 +0300

    claims updated and authorization changed to use permissions

--- internal/infrastructure/middleware/auth_middleware.go
+++ internal/infrastructure/middleware/auth_middleware.go
@@ -1,7 +1,9 @@
 package middleware
 
 import (
+	"fmt"
 	"net/http"
+	"slices"
 	"strings"
 
 	"github.com/gin-gonic/gin"
@@ -25,26 +27,55 @@ func JwtAuthMiddleware(secretKey string) gin.HandlerFunc {
 
 		c.Set("userID", claims["userID"])
 		c.Set("role", claims["role"])
+		c.Set("branchID", claims["branchID"])
+		c.Set("departmentID", claims["departmentID"])
+		c.Set("permissions", claims["permissions"])
 		c.Next()
 	}
 }
 
-func AuthorizeRoles(allowedRoles ...string) gin.HandlerFunc {
+func AuthorizeRolesOrPermissions(allowedRoles []string, requiredPermission []string) gin.HandlerFunc {
 	return func(c *gin.Context) {
+		fmt.Println("inside auth role and permission")
 		roleValue, exists := c.Get("role")
 		if !exists {
 			c.AbortWithStatusJSON(http.StatusForbidden, gin.H{"message": "Role not found"})
 			return
 		}
 
+		fmt.Print("required permissions", requiredPermission)
 		role, ok := roleValue.(string)
 		if !ok {
 			c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"message": "Invalid role format"})
 			return
 		}
 
-		for _, allowed := range allowedRoles {
-			if role == allowed {
+		if slices.Contains(allowedRoles, role) {
+			c.Next()
+			return
+		}
+
+		perms, permsExist := c.Get("permissions")
+		if !permsExist {
+			c.AbortWithStatusJSON(http.StatusForbidden, gin.H{"message": "Permission not found"})
+			return
+		}
+
+		permissions, ok := perms.([]interface{})
+		if !ok {
+			c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"message": "Invalid permissions format"})
+			return
+		}
+
+		userPerms := make([]string, 0, len(permissions))
+		for _, p := range permissions {
+			if str, ok := p.(string); ok {
+				userPerms = append(userPerms, str)
+			}
+		}
+
+		for _, required := range requiredPermission {
+			if slices.Contains(userPerms, required) {
 				c.Next()
 				return
 			}
